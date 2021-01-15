1741cbcb1cb157d64b4a62adb1e31954fdcd81d5
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-15 09:03:27 -0300

    Initial Commit

---
+++ Swift UI Principios/Swift_UI_PrincipiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_PrincipiosApp.swift
+//  Swift UI Principios
+//
+//  Created by João Gabriel Dourado Cervo on 15/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_PrincipiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
