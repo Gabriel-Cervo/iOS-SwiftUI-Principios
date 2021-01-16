ebffdee70c6fa193deb7637437a37db1919c6b8b
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-16 10:23:15 -0300

    Add template de lista de landmarks

---
+++ Swift UI Principios/View/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Swift UI Principios
+//
+//  Created by João Gabriel Dourado Cervo on 16/01/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
